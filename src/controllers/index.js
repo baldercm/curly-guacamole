a735548def132234e55b1cb4df46c5d3c5abcb32
Bálder Carraté <baldercm@gmail.com>
2017-01-20 19:20:09 +0100

    Sample error handling

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,12 +3,16 @@ import {ApiError} from '../lib/errors'
 
 export default function(router) {
   router.get('/', api(hello))
+  router.get('/error', api(error))
 }
 
 async function hello(req, res) {
-  //let model = await Model.findOne().exec()
+  let model = await Model.findOne().exec()
 
-  //res.status(200).json(model)
+  res.status(200).json(model)
+}
+
+async function error() {
   throw new ApiError()
 }
 
--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -2,15 +2,10 @@ import {ApiError} from '../lib/errors'
 
 export default function() {
   return function(err, req, res, next) {
-    console.log(1)
     if (!res.headersSent) {
-      console.log(2)
-      console.log(err)
       if (err instanceof ApiError) {
-        console.log(3)
         res.status(err.status || 400).json({ error: err.message, code: err.code })
       } else {
-        console.log(4)
         res.status(err.status || 500).json({ error: 'Internal Server Error' })
       }
     }
