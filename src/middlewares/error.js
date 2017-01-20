f54dea57737a2bb9fb55fa48291db383d2e04df4
Bálder Carraté <baldercm@gmail.com>
2017-01-20 21:53:10 +0100

    Refactor error handling middleware

--- src/middlewares/error.js
+++ src/middlewares/error.js
@@ -1,16 +1,18 @@
+import * as _     from 'lodash'
 import {ApiError} from '../lib/errors'
 
 export default function() {
   return function(err, req, res, next) {
+    _.set(res, 'app.error', err)
+
     if (!res.headersSent) {
       if (err instanceof ApiError) {
         res.status(err.status || 400).json({ error: err.message, code: err.code })
       } else {
         res.status(err.status || 500).json({ error: 'Internal Server Error' })
       }
+    } else {
+      next(err)
     }
-
-    res.app.error = err
-    return next(err)
   }
 }
