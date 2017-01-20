1ec16d7be54a9fdde39f89b3c80b7bafc6a93b01
Bálder Carraté <baldercm@gmail.com>
2017-01-20 21:09:30 +0100

    Basic access logger setup

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,6 +1,12 @@
-import winston from 'winston'
+import * as _   from 'lodash'
+import winston  from 'winston'
+import morgan   from 'morgan'
 
-function start(options) {
+const options = {}
+
+function start(opts) {
+  _.assign(options, opts)
+
   winston.configure({
     transports: [
       new (winston.transports.Console)({
@@ -11,10 +17,38 @@ function start(options) {
       new (winston.transports.File)({
         level: options.main.file.level,
         filename: options.main.file.filename,
-        logstash: true,
+        json: options.access.file.json,
+        logstash: options.main.file.json,
       }),
     ]
   })
 }
 
-export {start}
+const accessLogger = function(format, args) {
+  let logger = new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)({
+        level: options.access.console.level,
+        json: options.access.console.json,
+        stringify: options.access.console.stringify
+      }),
+      new (winston.transports.File)({
+        level: options.access.file.level,
+        filename: options.access.file.filename,
+        json: options.access.file.json,
+        logstash: options.access.file.json,
+      }),
+    ]
+  })
+
+  let accessLogStream = {
+    write: function(message){
+      message = _.replace(message, /(?:\r\n|\r|\n)/g, '')
+      logger.info(message)
+    }
+  }
+
+  return morgan(format, _.defaults(args, {stream: accessLogStream}))
+}
+
+export {start, accessLogger}
