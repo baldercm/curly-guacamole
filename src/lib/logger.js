dfa2af490d38bec0af13861c70e255d25f871ac3
Bálder Carraté <baldercm@gmail.com>
2017-01-20 21:54:11 +0100

    Basic API logger

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,6 +1,7 @@
-import * as _   from 'lodash'
-import winston  from 'winston'
-import morgan   from 'morgan'
+import * as _     from 'lodash'
+import winston    from 'winston'
+import morgan     from 'morgan'
+import onFinished from 'on-finished'
 
 const options = {}
 
@@ -24,7 +25,7 @@ function start(opts) {
   })
 }
 
-const accessLogger = function(format, args) {
+function accessLogger(format, args) {
   let logger = new (winston.Logger)({
     transports: [
       new (winston.transports.Console)({
@@ -51,4 +52,62 @@ const accessLogger = function(format, args) {
   return morgan(format, _.defaults(args, {stream: accessLogStream}))
 }
 
-export {start, accessLogger}
+function apiLogger(options) {
+  const apiLogger = new (winston.Logger)({
+    transports: [
+      new (winston.transports.Console)({
+        level: options.api.console.level,
+        json: options.api.console.json,
+        stringify: options.api.console.stringify
+      }),
+      new (winston.transports.File)({
+        level: options.api.file.level,
+        filename: options.api.file.filename,
+        json: options.api.file.json,
+        logstash: options.api.file.json,
+      }),
+    ]
+  })
+
+  return (req, res, next) => {
+    onFinished(res, writeApiLog)
+    next()
+
+
+    function writeApiLog() {
+      let message = {
+        req: {
+          method: req.method,
+          url: req.originalUrl,
+          body: req.body,
+          headers: req.headers,
+          ip: req.ip,
+        },
+        res: {
+          status: res.statusCode,
+        },
+      }
+
+      if (res.app.error) {
+        let err = res.app.error
+
+        message.error = {
+          type:     err.name,
+          message:  err.message,
+          stack:    err.stack,
+          causes:   err.causes ? _.map(err.causes, cause => cause.stack) : [_.get(err, 'cause.stack')],
+        }
+
+        if (res.statusCode >= 500) {
+          apiLogger.error(`Internal Server Error: ${res.app.error.message}`, message)
+        } else if (res.statusCode >= 400) {
+          apiLogger.error(res.app.error.message, message)
+        }
+      } else {
+        apiLogger.info(res.app.outcome, message)
+      }
+    }
+  }
+}
+
+export {start, accessLogger, apiLogger}
